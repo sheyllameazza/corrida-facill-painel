b4d1cb17683de4ad8963ad2c8c95b1187e422827
Sheyla <sheyllameazza@outlook.com>
2023-12-07 20:26:30 -0400

    Painel App Corrida Facil - Completo

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :segundos segundos.',
+    'unauthenticated' => 'Não Autenticado',
+    'reset_password' => 'Redefinir Senha',
+    'password_confirmation' => 'Confirmar Senha',
+    'email_password_reset_link' => 'Enviar E-mail de Redefinição de Senha',
+    'reset_password_description' => 'Digite seu endereço de e-mail e enviaremos um e-mail com instruções para redefinir sua senha.',
+];
